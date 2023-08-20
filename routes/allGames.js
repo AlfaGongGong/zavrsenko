13ffd03adb5216e4c2ceb4391f02625e286b97dc
Jasenko Bozinovic <jasenkobozinovic@gmail.com>
2023-08-20 20:07:23 +0200

    1

--- routes/allGames.js
+++ routes/allGames.js
@@ -10,5 +10,29 @@ router.get('/', async function (req, res, next) {
             next(err);
       }
 });
+router.post('/', async function (req, res, next) {
+      try {
+            res.json(await products_games.create(req.body));
+      } catch (err) {
+            console.error(`Error while creating game`, err.message);
+            next(err);
+      }
+});
+router.put('/:id', async function (req, res, next) {
+      try {
+            res.json(await products_games.update(req.params.id, req.body));
+      } catch (err) {
+            console.error(`Error while updating game`, err.message);
+            next(err);
+      }
+});
 
+router.delete('/:id', async function (req, res, next) {
+      try {
+            res.json(await products_games.remove(req.params.id));
+      } catch (err) {
+            console.error(`Error while deleting game`, err.message);
+            next(err);
+      }
+});
 module.exports = router;
