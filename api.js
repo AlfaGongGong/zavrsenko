a176d09090d220c657e94d6ff4a28d0c0e5b26a2
Jasenko Bozinovic <jasenkobozinovic@gmail.com>
2023-08-22 04:13:37 +0200

    1

--- api.js
+++ api.js
@@ -6,33 +6,68 @@ const mysql = require('mysql2');
 const logger = require('winston');
 const cron = require('node-cron');
 const _ = require('lodash');
+const cors = require('cors');
 
 const app = express();
 const winston = require('winston');
 
-app.use(express());
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 3000;
 
 logger.add(new winston.transports.File({ filename: './logs/api.log' }));
 // MySQL connection
 const connection = mysql.createConnection(
       config = {
             host: 'localhost',
-            user: 'admin',
-            password: 'admin',
+            user: 'root',
+            password: '',
             database: 'gg_database',
-            port: 3000,
+            port: 3306,
             connectTimeout: 60000,
 
       }
 );
+connection.connect((err) => {
+      if (err) {
+            logger.error(err);
+            console.log(err);
+      }
+      logger.info('Connected to database');
+      console.log('Connected to database');
+});
 
 // Create connection pool
-const pool = mysql.createPool({
-      host: 'localhost',
-      port: 3000,
-      user: 'admin',
-      password: 'admin',
-      database: 'gg_database'
+const pool = mysql.createPool(config);
+
+// Handle disconnects
+
+pool.on('connection', (connection) => {
+      connection.on('error', (err) => {
+            if (!err.fatal) {
+                  connection.connect();
+            }
+      });
+});
+
+// Handle errors
+
+pool.on('error', (err) => {
+      console.error('DB connection pool error:', err);
+});
+
+// Make test query
+
+pool.query('SELECT * FROM games LIMIT 5 ', (err, results) => {
+      if (err) throw err;
+
+      console.log('Query results:', results);
+      logger.info('Query results:', results);
+
 });
 
 // Handle disconnects 
@@ -44,46 +79,16 @@ pool.on('connection', (connection) => {
       });
 });
 
-// Handle errors
-pool.on('error', (err) => {
-      console.error('DB connection pool error:', err);
-});
-
-// Make test query
-pool.query('SELECT 1', (err, results) => {
-      if (err) throw err;
-
-      console.log('Query results:', results);
-      logger.info('Query results:', results);
-
-});
-
-connection.connect((err) => {
-      if (err) {
-            logger.error(err);
-            console.log(err);
-      }
-      logger.info('Connected to database');
-      console.log('Connected to database');
-});
-
 const gamesList = {
 
       method: 'GET',
-      url: 'https://rawg-video-games-database.p.rapidapi.com/games',
-      headers: {
-            'X-RapidAPI-Key': 'f6d1f840a3msh19d03ebda2bde72p14ca93jsn16413aedab2d',
-            'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com'
-      }
+      url: 'https://api.rawg.io/api/games?key=922f629661bf480cadd2986542dfe6fa&platform=1&dates=2020-01-01,2023-01-01',
 };
 
-const gameDetails = {
+const topTen = {
       method: 'GET',
-      url: 'https://rawg-video-games-database.p.rapidapi.com/games/%7Bgame_pk%7D',
-      headers: {
-            'X-RapidAPI-Key': 'f6d1f840a3msh19d03ebda2bde72p14ca93jsn16413aedab2d',
-            'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com'
-      }
+      url: 'https://api.rawg.io/api/games?key=922f629661bf480cadd2986542dfe6fa&metacritic=70,100',
+
 };
 
 const upcomingGames = {
@@ -116,18 +121,14 @@ const freeGames = {
 app.set('view engine', 'ejs');
 
 // API functions 
-const allGames = async () => {
+const getGames = () => {
+      return axios.request(gamesList)
+            .then(resp => resp.data);
+}
+
+const topList = async () => {
       try {
-            const resp = await axios.request(gamesList);
-            return resp.data;
-      } catch (error) {
-            logger.error(error);
-            return error;
-      }
-};
-const details = async () => {
-      try {
-            const resp = await axios.request(gameDetails);
+            const resp = await axios.request(topTen);
             return resp.data;
       } catch (error) {
             logger.error(error);
@@ -169,43 +170,57 @@ const getFreeGames = async () => {
 };
 
 // Sync function
-const syncData = async () => {
-      try {
-            const [allGames, details, getUpcoming, getReviews, getFreeGames] = await Promise.all([allGames(), details(), getUpcoming(), getReviews(), getFreeGames()]);
+try {
+      const syncData = async () => await Promise.all([
+            getGames(),
+            getReviews(),
+            getUpcoming(),
+            getFreeGames(),
+            topList(),
+            getReviews()
+      ]);
+      const allGames = syncData();
 
-            const dedupedAllGames = _.uniqBy(allGames, 'id');
-            const dedupedDetails = _.uniqBy(details, 'id');
-            const dedupedGetUpcoming = _.uniqBy(getUpcoming, 'id');
-            const dedupedGetReviews = _.uniqBy(getReviews, 'id');
-            const dedupedGetFreeGames = _.uniqBy(getFreeGames, 'id');
+      logger.info('Synced data');
 
-            connection.query('CREATE NEW ROWS IF NOT EXISTS INSERT INTO products_games SET ? ON DUPLICATE KEY UPDATE text=VALUES(text)', dedupedAllGames);
-            connection.query('CREATE NEW ROWS IF NOT EXISTS INSERT INTO details SET? ON DUPLICATE KEY UPDATE text=VALUES(text)', dedupedDetails);
-            connection.query('CREATE NEW ROWS IF NOT EXISTS INSERT INTO upcoming SET? ON DUPLICATE KEY UPDATE text=VALUES(text)', dedupedGetUpcoming);
-            connection.query('CREATE NEW ROWS IF NOT EXISTS INSERT INTO reviews SET? ON DUPLICATE KEY UPDATE text=VALUES(text)', dedupedGetReviews);
-            connection.query('CREATE NEW ROWS IF NOT EXISTS INSERT INTO free SET? ON DUPLICATE KEY UPDATE text=VALUES(text)', dedupedGetFreeGames);
+      console.log(allGames);
 
-            logger.info('Synced data');
+      const dedupedAllGames = _.uniqBy(allGames, 'id');
+      const dedupedDetails = _.uniqBy(details, 'id');
+      const dedupedGetUpcoming = _.uniqBy(getUpcoming, 'id');
+      const dedupedGetReviews = _.uniqBy(getReviews, 'id');
+      const dedupedGetFreeGames = _.uniqBy(getFreeGames, 'id');
 
-      } catch (error) {
-            logger.error(error);
-            return error;
-      }
+      connection.query('CREATE NEW ROWS IF NOT EXISTS INSERT INTO products_games SET ? ON DUPLICATE KEY UPDATE text=VALUES(text)', dedupedAllGames);
+      connection.query('CREATE NEW ROWS IF NOT EXISTS INSERT INTO details SET? ON DUPLICATE KEY UPDATE text=VALUES(text)', dedupedDetails);
+      connection.query('CREATE NEW ROWS IF NOT EXISTS INSERT INTO upcoming SET? ON DUPLICATE KEY UPDATE text=VALUES(text)', dedupedGetUpcoming);
+      connection.query('CREATE NEW ROWS IF NOT EXISTS INSERT INTO reviews SET? ON DUPLICATE KEY UPDATE text=VALUES(text)', dedupedGetReviews);
+      connection.query('CREATE NEW ROWS IF NOT EXISTS INSERT INTO free SET? ON DUPLICATE KEY UPDATE text=VALUES(text)', dedupedGetFreeGames);
 
-      cron.schedule('* *10 * * *', () => {
-            syncData();
-      })
-};
+      logger.info('Synced data');
+
+} catch (error) {
+      logger.error(error);
+      return error;
+}
+
+cron.schedule('*/5 * * * *', () => {
+      syncData();
+});
 
 // Product card route
-app.get('/card', async (req, res) => {
-      const result = await connection.query('SELECT * FROM games_products');
+app.get('/games', async (req, res) => {
+      const games = await getGamesFromDB();
 
-      res.render('products', { products: result });
+      res.render('games', { games });
+})
+
+app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+      logger.info(`Server running on port ${port}`);
 
 });
 
-app.listen(3000);
 console.log(connection);
 
 module.exports = {
