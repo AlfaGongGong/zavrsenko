b2bf79fc75a9b39ae7a31b1f07622b14b5054b3b
Jasenko Bozinovic <jasenkobozinovic@gmail.com>
2023-09-10 00:52:32 +0200

    1

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
 
 
-const slidesContainer = document.getElementById("slides-container");
-const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
-nextButton.addEventListener("click", () => {
-	const slideWidth = slide.clientWidth;
-	slidesContainer.scrollLeft += slideWidth;
-});
-prevButton.addEventListener("click", () => {
-	const slideWidth = slide.clientWidth;
-	slidesContainer.scrollLeft -= slideWidth;
-});
-
 /* animate genre icons */
-document.querySelectorAll(".icon").forEach((element) => {
+document.querySelectorAll(".icon-container").forEach((element) => {
 	element.addEventListener("mouseover", () => {
 		element.classList.add("animate__animated", "animate__flip");
 	});
