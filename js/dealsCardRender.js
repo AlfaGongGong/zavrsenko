6fe7193eda8208fb9a3db915e125c6eae0b5c8c1
Jasenko Bozinovic <jasenkobozinovic@gmail.com>
2023-10-04 19:43:23 +0200

    1

--- js/dealsCardRender.js
+++ js/dealsCardRender.js
@@ -1,19 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("http://localhost:3000/deals")
+    .then((response) => response.json())
+    .then((dealsCards) => {
+      const dealsCardsContainer = document.querySelector(
+        ".dealsCardsContainer"
+      );
 
-document.addEventListener('DOMContentLoaded', () => {
-      fetch('http://localhost:3000/deals')
-            .then(response => response.json())
-            .then(dealsCards => {
-                  const dealsCardsContainer = document.querySelector('.dealsCardsContainer');
+      // Shuffle the array of dealsCards
+      dealsCards = shuffle(dealsCards);
 
-                  // Shuffle the array of dealsCards
-                  dealsCards = shuffle(dealsCards);
-
-                  // Loop through the first 6 dealsCards and append them to the container
-                  dealsCards.slice(0, 6).forEach(card => {
-                        // Create a product card element
-                        const cardElement = document.createElement('div');
-                        cardElement.classList.add('product-card', 'col-md-4');
-                        cardElement.innerHTML = `
+      // Loop through the first 6 dealsCards and append them to the container
+      dealsCards.slice(0, 6).forEach((card) => {
+        // Create a product card element
+        const cardElement = document.createElement("div");
+        cardElement.classList.add("product-card", "col-md-4");
+        cardElement.innerHTML = `
     <img src="${card.image}" alt="${replaceImg}" class="product-image"/> 
     <h2 class="product-name">${card.title}</h2>
 <div class="card-info">
@@ -28,32 +29,31 @@ lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
 
 `;
 
-                        // Append the card to the container
-                        dealsCardsContainer.appendChild(cardElement);
-                  });
-            })
-            .catch(error => {
-                  console.error('Error fetching deals cards:', error);
-            });
+        // Append the card to the container
+        dealsCardsContainer.appendChild(cardElement);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching deals cards:", error);
+    });
 
-      // Shuffle function to shuffle the array of dealsCards
-      function shuffle(array) {
-            let currentIndex = array.length;
-            let temporaryValue, randomIndex;
+  // Shuffle function to shuffle the array of dealsCards
+  function shuffle(array) {
+    let currentIndex = array.length;
+    let temporaryValue, randomIndex;
 
-            // While there remain elements to shuffle...
-            while (0 !== currentIndex) {
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
 
-                  // Pick a remaining element...
-                  randomIndex = Math.floor(Math.random() * currentIndex);
-                  currentIndex -= 1;
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
 
-                  // And swap it with the current element.
-                  temporaryValue = array[currentIndex];
-                  array[currentIndex] = array[randomIndex];
-                  array[randomIndex] = temporaryValue;
-            }
-
-            return array;
-      }
+    return array;
+  }
 });
