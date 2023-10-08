e69d1235e0c6eb863b6e2ea1cb9a180eb5d68c71
Jasenko Bozinovic <jasenkobozinovic@gmail.com>
2023-10-08 03:40:40 +0200

    1

--- mysqlDB/functions.sql
+++
@@ -1,7 +0,0 @@
-CREATE FUNCTION `GenerateRandomPrice` ()
-RETURNS DECIMAL(10,2)
-BEGIN
-    DECLARE random_value DECIMAL(10,2);
-    SET random_value = ROUND(20 + RAND() * (150 - 20), 2);
-    RETURN random_value;
-END$$
