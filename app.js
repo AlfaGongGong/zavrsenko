9aad77020f945e306ab86a0d2e694ca630ed150e
Jasenko Bozinovic <jasenkobozinovic@gmail.com>
2023-10-16 18:28:32 +0200

    1

--- app.js
+++ app.js
@@ -2,18 +2,20 @@ const express = require("express");
 const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
 const app = express();
+require("dotenv").config();
+
+
 app.use(bodyParser.json());
 app.use(cors());
 
 // Database create connection
 const dbConfig = {
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "gg_database",
-  port: 3306,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_SERVER
 };
 
 // Routes
@@ -29,7 +31,7 @@ connection.connect((error) => {
     return;
   }
   console.log("Connected to the database:", dbConfig.database);
-  app.listen(8080, () => {
-    console.log(`Server running on port 8080`);
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
   });
 });
