6fe85a8b2d82a684fedbddb92b138dc11a66d9fe
Jasenko Bozinovic <jasenkobozinovic@gmail.com>
2023-11-27 22:32:06 +0100

    1
    1

--- js/loremIpsum.js
+++ js/loremIpsum.js
@@ -1,4 +1,4 @@
-const createLoremIpsumElement = () => {
+function getRandomLoremIpsumText() {
   const loremsIpsums = [
     "Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.",
     "Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.",
@@ -8,8 +8,9 @@ const createLoremIpsumElement = () => {
     "Lookout flogging bilge rat main sheet bilge water nipper fluke to go on account heave down clap of thunder. Reef sails six pounders skysail code of conduct sloop cog Yellow Jack gunwalls grog blossom starboard. Swab black jack ahoy Brethren of the Coast schooner poop deck main sheet topmast furl marooned."
   ];
 
-  const loremIpsumText = loremsIpsums[Math.floor(Math.random() * loremsIpsums.length)];
-  console.log(loremIpsumText);
+  const randomIndex = Math.floor(Math.random() * loremsIpsums.length);
+  return loremsIpsums[randomIndex];
 }
 
-module.exports = createLoremIpsumElement;
+const randomLoremIpsumText = getRandomLoremIpsumText();
+
